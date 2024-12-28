a5d88760380fdeb94b03da28825a173a82236aae
5hes <553919500@qq.com>
2024-12-28 10:24:12 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -4,57 +4,57 @@ set_languages("c17", "cxx20")
 add_rules("mode.release", "mode.debug")
 
 if not is_os("windows") then
-	add_defines("_POSIX_C_SOURCE=200809L")
+    add_defines("_POSIX_C_SOURCE=200809L")
 end
 
 rule("static_binary")
-	on_load(function (target)
-		if target:is_plat("linux") then
-			target:add("ldflags", "-static-pie")
-		elseif target:is_plat("mingw") then
-			target:add("ldflags", "-static")
-		end
-	end)
+    on_load(function (target)
+        if target:is_plat("linux") then
+            target:add("ldflags", "-static-pie")
+        elseif target:is_plat("mingw") then
+            target:add("ldflags", "-static")
+        end
+    end)
 
 target("merge-otd")
-	set_kind("binary")
-	add_rules("static_binary")
-	add_deps("clipp", "fmt", "json", "nowide", "spdlog")
-	add_deps("intl")
-        add_includedirs("3rdparty/spdlog/include", {public = true})
-	add_files(
-		"src/merge-name.cpp",
-		"src/merge-otd.cpp",
-		"src/ps2tt.cpp",
-		"src/tt2ps.cpp")
+    set_kind("binary")
+    add_rules("static_binary")
+    add_deps("clipp", "fmt", "json", "nowide", "spdlog")
+    add_deps("intl")
+    add_includedirs("3rdparty/spdlog/include", {public = true})
+    add_files(
+        "src/merge-name.cpp",
+        "src/merge-otd.cpp",
+        "src/ps2tt.cpp",
+        "src/tt2ps.cpp")
 
 target("otfccbuild")
-	set_kind("binary")
-	add_rules("static_binary")
-	add_deps("clipp", "fmt", "nowide", "spdlog")
-	add_deps("intl", "otfcc")
-	add_deps("config")
-        add_includedirs("3rdparty/spdlog/include", {public = true})
-	add_files(
-		"src/otfccbuild.cpp",
-		"src/stopwatch.cpp")
+    set_kind("binary")
+    add_rules("static_binary")
+    add_deps("clipp", "fmt", "nowide", "spdlog")
+    add_deps("intl", "otfcc")
+    add_deps("config")
+    add_includedirs("3rdparty/spdlog/include", {public = true})
+    add_files(
+        "src/otfccbuild.cpp",
+        "src/stopwatch.cpp")
 
 target("otfccdump")
-	set_kind("binary")
-	add_rules("static_binary")
-	add_deps("clipp", "fmt", "nowide", "spdlog")
-	add_deps("intl", "otfcc")
-	add_deps("config")
-        add_includedirs("3rdparty/spdlog/include", {public = true})
-	add_files(
-		"src/otfccdump.cpp",
-		"src/stopwatch.cpp")
+    set_kind("binary")
+    add_rules("static_binary")
+    add_deps("clipp", "fmt", "nowide", "spdlog")
+    add_deps("intl", "otfcc")
+    add_deps("config")
+    add_includedirs("3rdparty/spdlog/include", {public = true})
+    add_files(
+        "src/otfccdump.cpp",
+        "src/stopwatch.cpp")
 
 target("config")
-	set_kind("phony")
-	add_includedirs("$(buildir)", {public = true})
-	add_configfiles("config/config.h.in", {variables = variables})
-	add_configfiles("config/config.sh.in", {variables = variables})
+    set_kind("phony")
+    add_includedirs("$(buildir)", {public = true})
+    add_configfiles("config/config.h.in", {variables = variables})
+    add_configfiles("config/config.sh.in", {variables = variables})
 
 includes("3rdparty/clipp")
 includes("3rdparty/fmt")
